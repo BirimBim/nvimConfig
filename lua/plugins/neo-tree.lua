9f0943538b73798b28b2b4448cd41697b64b81ee
BirimBim <125955981+BirimBim@users.noreply.github.com>
2024-10-25 23:49:18 -0700

    creating repo

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+    },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      enable_git_status = true,
+      window = {
+        position = "left",
+        width = 30
+      }
+    })
+  end
+}
